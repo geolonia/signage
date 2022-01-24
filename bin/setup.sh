717ed713122f0736b133d15e9156d7aeae3dd426
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2022-01-24 14:20:33 +0900

    added setup script

---
+++ bin/setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sudo raspi-config nonint do_change_locale ja_JP.UTF-8
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt purge wolfram-engine scratch nuscratch sonic-pi idle3 smartsim java-common libreoffice* -y
+
+sudo apt install unclutter -y
+
+sudo apt clean
+sudo apt autoremove -y
+
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt install nodejs
+
+sudo rm -f /etc/xdg/lxsession/LXDE-pi/sshpwd.sh
+
+mkdir -p ~/.config/lxsession/LXDE-pi
+mkdir -p ~/.config/openbox
+
+rm -fr
+git clone https://github.com/geolonia/signage.git app
+npm install
+
+echo "@openbox-session" > ~/.config/lxsession/LXDE-pi/autostart
+
+cat <<EOS > ~/.config/openbox/autostart
+xset -dpms &     # Disable DPMS (Energy Star) features
+xset s off &     # Disable screensaver
+xset s noblank & # Don't blank video device
+
+unclutter -idle 0 &
+
+cd ~/app
+git pull
+npm install
+npm run serve
+
+chromium-browser --noerrdialogs --disable-infobars --gpu --gpu-launcher --in-process-gpu --ignore-gpu-blacklist --ignore-gpu-blocklist --kiosk https://localhost:3000
+EOS
