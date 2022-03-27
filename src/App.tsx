e64fedc6341deb6599a7b64b6666cf1b1f9ff97a
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2022-03-27 11:29:03 +0900

    some fix

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,24 @@ const App = () => {
         // nothing to do
       })
     })
+
+    map.on('load', () => {
+      const now = (new Date()).getTime();
+      const r   = 0;// relative time: -36~+11
+      const t1  = (new Date(now - now%(5*60*1000) + (r<0?r:(r?-1:0))*5*60*1000)).toISOString().slice(0,19).replace(/[^0-9]/g,"");
+      const t2  = (r<0)?t1:(new Date(now - now%(5*60*1000) + r*5*60*1000)).toISOString().slice(0,19).replace(/[^0-9]/g,"");
+      console.log(t1)
+      console.log(t2)
+      map.addSource('weather', {
+        type: 'raster',
+        tiles: [`https://www.jma.go.jp/bosai/jmatile/data/nowc/${t1}/none/${t2}/surf/hrpns/{z}/{x}/{y}.png`],
+      })
+      map.addLayer({
+        id: 'weather',
+        type: 'raster',
+        source: 'weather'
+      })
+    })
   }, [mapContainer])
 
   return (
